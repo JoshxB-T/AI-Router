b445492b077db73fa3b2fa16604d305abe04f56e
JoshxB-T <joshtorrez320@gmail.com>
2026-02-23 13:23:45 -0600

    Updated file for database retrieval

--- backend/app/datatier.py
+++ backend/app/datatier.py
@@ -3,4 +3,21 @@ import sqlite3
 #
 # Function
 #
+def fetch_all_rows(dbConn):
+    query =
+    """
+    SELECT
 
+    """
+
+    return None
+
+
+def fetch_n_rows(dbConn):
+    query =
+    """
+    SELECT
+
+    """
+
+    return None
