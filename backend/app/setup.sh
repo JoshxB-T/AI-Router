437d9c67977faef7067fffa9235ecbcf0b8ce3a4
JoshxB-T <joshtorrez320@gmail.com>
2026-02-26 14:36:31 -0600

    Added variable for database file

--- backend/app/setup.sh
+++ backend/app/setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 PY="python3"
+DB_FILE="dbconversion.py"
 
 echo ">>> unzipping video_games.zip"
 cd data
 unzip video_games.zip
 echo ">>> complete"
 
-cd ..
-
 echo ">>> building video_games.db"
-$PY dbconversion.py
+cd ..
+$PY $DB_FILE
 echo ">>> complete"
