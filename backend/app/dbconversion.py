5602f6ae98ba622db0e4bfed43a597eca0d89fd9
JoshxB-T <joshtorrez320@gmail.com>
2026-02-26 08:30:47 -0600

    Added compressed csv data and python script to build database

---
+++ backend/app/dbconversion.py
@@ -0,0 +1,63 @@
+import csv
+import sqlite3
+
+
+CSV_PATH = "data/video_games.csv"
+DB_PATH = "data/video_games.db"
+
+
+def read_csv(fname):
+    dbConn = sqlite3.connect(DB_PATH)
+    dbCursor = dbConn.cursor()
+
+    with open(fname, mode='r', newline='', encoding='utf-8') as f:
+        csv_content = csv.DictReader(f)
+
+        for row in csv_content:
+            query = f"""
+            insert into video_games (Name, Platform, Year_of_Release, Genre, Publisher, NA_Sales, EU_Sales, JP_Sales, Other_Sales, Global_Sales)
+            values ( (?), (?), (?), (?), (?), (?), (?), (?), (?), (?) );
+            """
+            params = (row['Name'], row['Platform'], row['Year_of_Release'], row['Genre'], row['Publisher'], row['NA_Sales'], row['EU_Sales'], row['JP_Sales'], row['Other_Sales'], row['Global_Sales'])
+
+            dbCursor.execute(query, params)
+
+    dbConn.commit()
+    dbCursor.close()
+    dbConn.close()
+
+
+def create_db(fname):
+    dbConn = sqlite3.connect(fname)
+    dbCursor = dbConn.cursor()
+
+    query = """
+    create table if not exists video_games (
+        id integer primary key,
+        Name text not null,
+        Platform text not null,
+        Year_of_Release integer not null,
+        Genre text not null,
+        Publisher text not null,
+        NA_Sales integer not null,
+        EU_Sales integer not null,
+        JP_Sales integer not null,
+        Other_Sales integer not null,
+        Global_Sales integer not null
+    );
+    """
+
+    dbCursor.execute(query)
+
+    dbCursor.close()
+    dbConn.commit()
+    dbConn.close()
+
+
+def main():
+    create_db(DB_PATH)
+    read_csv(CSV_PATH)
+
+
+if __name__ == "__main__":
+    main()
